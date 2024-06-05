c7c945e26648d5e83b187b874849a2f27a12fa87
RUBIYASULTHANA <ridamaheen68@gmail.com>
2024-06-05 08:45:43 +0530

    events onclick added

---
+++ Events/calc.js
@@ -0,0 +1,43 @@
+function Addition(){
+    let a=eval(document.getElementById('t1').value)
+    let b=eval(document.getElementById('t2').value)
+    let c=eval(document.getElementById('t3').value)
+
+    let result=a+b+c;
+    document.getElementById('t4').value=result;
+}
+function subtraction(){
+    let a=eval(document.getElementById('t1').value)
+    let b=eval(document.getElementById('t2').value)
+    let c=eval(document.getElementById('t3').value)
+    let result=a-b-c;
+    document.getElementById('t4').value=result;
+}
+function Multiply(){
+    let a=eval(document.getElementById('t1').value)
+    let b=eval(document.getElementById('t2').value)
+    let c=eval(document.getElementById('t3').value)
+    let result=a*b*c;
+    document.getElementById('t4').value=result;
+}
+function Division(){
+    let a=eval(document.getElementById('t1').value)
+    let b=eval(document.getElementById('t2').value)
+    let c=eval(document.getElementById('t3').value)
+    let result=a/b/c;
+    document.getElementById('t4').value=result;
+}
+function Modulus(){
+    let a=eval(document.getElementById('t1').value)
+    let b=eval(document.getElementById('t2').value)
+    let c=eval(document.getElementById('t3').value)
+    let result=a%b%c;
+    document.getElementById('t4').value=result;
+}
+function Clear(){
+    document.getElementById('t1').value=""
+    document.getElementById('t2').value=""
+    document.getElementById('t3').value=""
+    document.getElementById('t4').value=""
+
+}
